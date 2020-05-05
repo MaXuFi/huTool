5b9fb6493337e8f0ac6a63565b67d95ad0e69e8d
MaXuFi <15707951907@139.com>
2020-05-06 00:20:47 +0800

    huTool依赖测试

---
+++ src/main/java/com/hutoolexample/demo_hutool/DemoHutoolApplication.java
@@ -0,0 +1,14 @@
+package com.hutoolexample.demo_hutool;
+
+import cn.hutool.crypto.SecureUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHutoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHutoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hutoolexample/demo_hutool/DemoHutoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutoolexample.demo_hutool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHutoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
